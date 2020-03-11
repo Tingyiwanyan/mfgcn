241e422cd714ad8da98f555843b0116063c8d38a
Tingyi <tiwanyan@gmail.com>
2020-03-11 19:06:20 -0400

    building kg

--- src/Kg_construction.py
+++ src/Kg_construction.py
@@ -27,18 +27,23 @@ class Kg_construct_ehr():
 
     def read_diagnosis(self):
         self.diag = pd.read_csv(self.diagnosis)
+        self.diag_ar = np.array(self.diag)
 
     def read_diagnosis_d(self):
         self.diag_d = pd.read_csv(self.diagnosis_d)
+        self.diag_d_ar = np.array(self.diag_d)
 
     def read_prescription(self):
         self.pres = pd.read_csv(self.prescription)
 
     def read_charteve(self):
         self.char = pd.read_csv(self.charteve,chunksize=10000)
+        self.char_ar = np.array(self.char.get_chunk())
+        self.num_char = self.char_ar.shape[0]
 
     def read_ditem(self):
         self.d_item = pd.read_csv(self.d_item)
+        self.d_item_ar = np.array(self.d_item)
 
     def read_noteevent(self):
         self.note = pd.read_csv(self.noteevents,chunksize=10000)
@@ -49,12 +54,29 @@ class Kg_construct_ehr():
 
     def create_kg(self):
         self.g = nx.DiGraph()
-        self.g.add_node()
+        for i in range(self.num_char):
+            patient_id = self.char_ar[i][1]
+            itemid = self.char_ar[i][4]
+            value = self.char_ar[i][8]
+            itemid_list = np.where(self.d_item_ar == itemid)
+            diag_list = np.where(self.diag_ar[:,1] = patient_id)
+            diag_icd9_list = self.diag_ar[:,4][diag_list]
+            diag_d_list = [np.where(self.diag_d_ar[:,1] == diag_icd9_list[x])[0] for x in range(diag_icd9_list.shape[0])]
+            """
+            Add patient node
+            """
+            self.g.add_node(patient_id, item_id=itemid)
+            self.g.add_node(patient_id, test_value=value)
+            self.g.add_node(patient_id, node_type='patient')
+            self.g.add_node(patient_id, itemid_list=itemid_list)
+            self.g.add_node(itemid, node_type='ICD9')
+            """
+            Add diagnosis ICD9 node
+            """
+            self.g.add_edge(patient_id, itemid, type='')
+
 
 
 if __name__ == "__main__":
     kg = Kg_construct_ehr()
-    kg.read_charteve()
-    kg.read_diagnosis()
-    kg.read_proc_icd()
 
